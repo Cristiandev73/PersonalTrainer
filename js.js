caf591014d1c83158f1549149dbbaa3874790961
Cristian <cristianbernardinodev@gmail.com>
2021-03-09 00:54:27 -0300

    Primeiro commit

---
+++ js.js
@@ -0,0 +1,15 @@
+$('.slider-principal').slick({
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 600,
+    fade: true,
+    adaptiveHeight: true,
+    cssEase: 'linear' 
+    
+  
+  });
+
+
+  
